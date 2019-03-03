b8ede27e581b690d3521779c71c7f5eaaef232dc
Lucas Ambrust <ambrust.lucas@gmail.com>
2019-03-03 15:52:23 -0300

    primeiro commit

---
+++ Course/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        //Consctructor #1
+        public Department()
+        {
+
+        }
+
+        //Constructor #2
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Course/Entities/HourContract.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Course.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        //Constructor #1
+        public HourContract()
+        {
+                
+        }
+
+        //Constructor #2
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        //method TotaValue()
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
